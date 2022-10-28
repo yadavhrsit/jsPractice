2dc31903d26fb3a14a8c48d5dc871f7463de25b8
Harshit <Inbox.HarshitYadav@gmail.com>
2022-10-28 13:03:55 +0530

    Update ajax.js

--- ajax.js
+++ ajax.js
@@ -206,50 +206,63 @@
 
 // Promisifying above code // XHR Using Promises
 
-const URL = "https://jsonplaceholder.typicode.com/posts";
-function sendRequest(method,url){
-    return new Promise((resolve, reject) => {
-        const xhr = new XMLHttpRequest();
-        xhr.open(method,url);
-        xhr.onload = function(){
-            if (xhr.status >=200 && xhr.status < 300){
-                resolve(xhr.response);
-            }
-            else{
-                reject(new Error("Something went wrong"));
-            }
-        }
-        xhr.onerror = ()=>{
-            reject(new Error("Network Error"));
-        }
+// const URL = "https://jsonplaceholder.typicode.com/posts";
+// function sendRequest(method,url){
+//     return new Promise((resolve, reject) => {
+//         const xhr = new XMLHttpRequest();
+//         xhr.open(method,url);
+//         xhr.onload = function(){
+//             if (xhr.status >=200 && xhr.status < 300){
+//                 resolve(xhr.response);
+//             }
+//             else{
+//                 reject(new Error("Something went wrong"));
+//             }
+//         }
+//         xhr.onerror = ()=>{
+//             reject(new Error("Network Error"));
+//         }
 
-        xhr.send();
-    })
-}
+//         xhr.send();
+//     })
+// }
 
-sendRequest("GET",URL)
-.then(response => {
-    const data = JSON.parse(response);
-    /* console.log(data); */
-    return data;
-}
-)
-.then(data =>{
-    const id = data[3].id;
-    return id;
-}
-)
-.then(id=>{
-    const url = URL+"/"+id;
-    return sendRequest("GET",url);
-}
-)
-.then(newResponse =>{
-    const newData = JSON.parse(newResponse);
-    console.log(newData);
-}
-)
+// sendRequest("GET",URL)
+// .then(response => {
+//     const data = JSON.parse(response);
+//     /* console.log(data); */
+//     return data;
+// }
+// )
+// .then(data =>{
+//     const id = data[3].id;
+//     return id;
+// }
+// )
+// .then(id=>{
+//     const url = URL+"/"+id;
+//     return sendRequest("GET",url);
+// }
+// )
+// .then(newResponse =>{
+//     const newData = JSON.parse(newResponse);
+//     console.log(newData);
+// }
+// )
+// .catch(error => {
+//     console.log(error);
+// })
 
-.catch(error => {
-    console.log(error);
-})
+
+                // fetch
+const URL = "https://jsonplaceholder.typicode.com/users";
+fetch(URL)
+            .then(response=>{
+                console.log(response);
+                const resp = response.json();
+                console.log(resp);
+                return resp;
+            })
+                .then(data =>{
+                    console.log(data);
+                });
